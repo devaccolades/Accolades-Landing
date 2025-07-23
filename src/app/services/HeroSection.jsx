9f6298ce507870127b2e72947c321bae1304b739
adil-accolades <adil.accolades@gmail.com>
2025-07-23 14:08:04 +0530

    font added

---
+++ src/app/services/HeroSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <section>
+        <div>
+            <h1>
+                Services
+            </h1>
+        </div>
+    </section>
+  )
+}
+
+export default HeroSection

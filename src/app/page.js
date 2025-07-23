2133348171f1f47ac89443511198994085bfd496
Accolades Integrated <accoladesintegrated@Accoladess-iMac.local>
2025-07-23 17:44:27 +0530

    home page added

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import React from "react";
 import HeroSection from "./(home)/HeroSection";
 import Accolades from "./(home)/Accolades";
 import OurServices from "./(home)/OurServices";
+import Count from "./(home)/Count";
 
 function page() {
   return (
@@ -9,6 +10,7 @@ function page() {
       <HeroSection />
       <Accolades />
       <OurServices />
+      <Count />
     </>
   );
 }

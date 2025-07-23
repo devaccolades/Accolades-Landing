664037f2453613ac01a3e3e3b4ffe372f3e2eb22
adil-accolades <adil.accolades@gmail.com>
2025-07-23 16:04:16 +0530

    small push

--- src/app/services/HeroSection.jsx
+++ src/app/services/HeroSection.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from "react";
 
 function HeroSection() {
   return (
-    <section>
-        <div>
-            <h1>
-                Services
-            </h1>
-        </div>
+    <section className="h-[60vh] w-full">
+      <div className="flex flex-col items-center justify-center bg-gray-500 w-[80%] mx-auto p-4 rounded-lg">
+        <h1 className="font-monst text-[86px] font-bold leading-[130%] text-[#3FB4BA]">
+          Services
+        </h1>
+        <p className="max-w-6xl text-center mt-[30px]">
+          Lorem ipsum dolor sit amet consectetur. Lobortis enim ante enim metus
+          et ultricies interdum. Netus malesuada fermentum enim at sed morbi dis
+          in lorem. Proin sapien tincidunt sed malesuada consequat pellentesque
+          ipsum. Nibh lectus cras id metus magna.
+        </p>
+      </div>
     </section>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;

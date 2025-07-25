709c193cc18c8d7f4774201a0d0a0dd6a1bbf534
Accolades Integrated <accoladesintegrated@Accoladess-iMac.local>
2025-07-25 14:49:59 +0530

    updated

---
+++ src/app/services/digital-marketing/google-ads/HeroSection.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <main className=" bg-[#F1F1F1]">
+      <section className="containers">
+        <h1
+          className="bg-white text-center text-[#3FB4BA] text-[86px] font-mont py-5 rounded-[30px] font-extrabold
+        leading-[100%]"
+        >
+          Google Ads
+        </h1>
+        <div className="grid grid-cols-2 py-5">
+          <div className="flex flex-col font-mont bg-white p-8 rounded-[30px]">
+            <h3>OUR COMMUNITY</h3>
+            <h3 className="text-[#3FB4BA] text-[40px] font-extrabold">
+              We Are Optimists Who Love To Work Together
+            </h3>
+            <p>
+              Google Ad is an online advertising service developed by Google to
+              help marketers contact their likely clients instantly. As we are a
+              precise google Adwords company in kochi, Kerala, and can
+              efficiently deliver a fundamental change in traffic statics and
+              progressively improve business within hours after a Google Ad
+              account is naturally created.
+            </p>
+            <p>
+              To naturally obtain an appropriate position in business
+              strategies, it’s mandatory to make use of Google Ads. Just traffic
+              is not the familiar mantra towards a phenomenal success; relevant
+              traffic naturally needs climbing the stairs of considerable
+              success. Now we are the top Google ads partner Kerala. PPC
+              advertising remains comfortably as one such specific mode, through
+              which traffic static can be amended and also increase the
+              considerable number of successful conversions on those official
+              websites which leads to more sales and inquiries.
+            </p>
+            <p>Google Certified AddWords partner in Kerala</p>
+          </div>
+          <div></div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default HeroSection;
--- src/app/services/digital-marketing/google-ads/page.jsx
+++ src/app/services/digital-marketing/google-ads/page.jsx
@@ -1,11 +1,13 @@
 import Footer from "@/component/Footer";
 import Navbar from "@/component/Navbar";
 import React from "react";
+import HeroSection from "./HeroSection";
 
 const page = () => {
   return (
     <>
       <Navbar />
+      <HeroSection />
       <Footer />
     </>
   );

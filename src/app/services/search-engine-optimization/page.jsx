b231b331a26cb11a9ac16d66a649a7086b3fbc9b
Accolades Integrated <accoladesintegrated@Accoladess-iMac.local>
2025-07-25 14:07:28 +0530

    services inner

---
+++ src/app/services/search-engine-optimization/page.jsx
@@ -0,0 +1,14 @@
+import Footer from "@/component/Footer";
+import Navbar from "@/component/Navbar";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+};
+
+export default page;

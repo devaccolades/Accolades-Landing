b4a2b6bb69aa531da92ec983f6ea6b4519115230
Accolades Integrated <accoladesintegrated@Accoladess-iMac.local>
2025-07-25 17:23:59 +0530

    seo page

--- src/app/services/digital-marketing/google-ads/page.jsx
+++ src/app/services/digital-marketing/google-ads/page.jsx
@@ -2,13 +2,20 @@ import Footer from "@/component/Footer";
 import Navbar from "@/component/Navbar";
 import React from "react";
 import HeroSection from "./HeroSection";
+import Types from "./types";
+import Cards from "./Cards";
 
 const page = () => {
   return (
     <>
-      <Navbar />
-      <HeroSection />
-      <Footer />
+      <main className="bg-[#F1F1F1]">
+        <Navbar />
+        <HeroSection />
+        <Cards />
+      </main>
+      <main className="-mt-[80px]">
+        <Footer />
+      </main>
     </>
   );
 };

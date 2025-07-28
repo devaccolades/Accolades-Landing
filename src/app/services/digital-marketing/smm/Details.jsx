5cc2fbeaf6fe515fb063ccafcba4c5307c6973e7
Accolades Integrated <accoladesintegrated@Accoladess-iMac.local>
2025-07-28 11:36:24 +0530

    updated

---
+++ src/app/services/digital-marketing/smm/Details.jsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import React from "react";
+
+import data from "../../../../../public/google/Group 249.svg";
+import mobile from "../../../../../public/google/OBJECTS (2).svg";
+
+const Details = () => {
+  return (
+    <section className="containers">
+      <div className="bg-white p-[5%] md:p-[3%] rounded-[20px] md:rounded-[30px]">
+        <p className="font-mont text-[16px] md:text-[18px] font-bold leading-[156%] pb-10">
+          We are experienced professionals, who sketch out efficient Social
+          Media Marketing strategy and Packages to help you.
+        </p>
+        <Image src={data} alt="cover" className="hidden md:block " />
+        <Image src={mobile} alt="cover" className=" md:hidden " />
+        <div className="pt-5 block md:hidden space-y-2">
+          <p className="bg-[#F2FBFB] border border-[#BFE1DE] rounded-[20px] text-center text-[13px] p-2">
+            Boost your sales and leads
+          </p>
+          <p className="bg-[#F2FBFB] border border-[#BFE1DE] rounded-[20px] text-center text-[13px] p-2">
+            Improve search engine rankings
+          </p>
+          <p className="bg-[#F2FBFB] border border-[#BFE1DE] rounded-[20px] text-center text-[13px] p-2">
+            To actively involve with your wide range of customers
+          </p>
+          <p className="bg-[#F2FBFB] border border-[#BFE1DE] rounded-[20px] text-center text-[13px] p-2">
+            Increase awareness about your brand
+          </p>
+          <p className="bg-[#F2FBFB] border border-[#BFE1DE] rounded-[20px] text-center text-[13px] p-2">
+            Choose the best social media platform to cultivate an engaging
+            customer base which fits the personality of your brand
+          </p>
+          <p className="bg-[#F2FBFB] border border-[#BFE1DE] rounded-[20px] text-center text-[13px] p-2">
+            Identify key influencers
+          </p>
+          <p className="bg-[#F2FBFB] border border-[#BFE1DE] rounded-[20px] text-center text-[13px] p-2">
+            Monitor engagements and backlinks you receive through campaigns
+            (likes, shares)
+          </p>
+        </div>
+        <p className="font-mont text-[13px] md:text-[16px] font-normal leading-[156%] pt-10">
+          We are one of the leading Best Social media marketing agency Kochi and
+          also Google AdWords partner in kochi , as we have extended our Social
+          Media Management sector to Calicut also. The key element to success in
+          your business is, to realize the potential for growth in this platform
+          by, utilizing the opportunities that is offered from our proficient
+          team in Social Media Marketing.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Details;
--- src/app/services/digital-marketing/smm/page.jsx
+++ src/app/services/digital-marketing/smm/page.jsx
@@ -2,16 +2,20 @@ import Footer from "@/component/Footer";
 import Navbar from "@/component/Navbar";
 import React from "react";
 import HeroSection from "./HeroSection";
-
-import Cards from "./Cards";
+import Details from "./Details";
 
 const page = () => {
   return (
     <>
-      <main className="bg-[#F1F1F1]">
+      <main
+        className="bg-[#F1F1F1] pb-40"
+        style={{
+          background: "linear-gradient(180deg, #F1F1F1 0%, #0199A4 100%)",
+        }}
+      >
         <Navbar />
         <HeroSection />
-        <Cards />
+        <Details />
       </main>
       <main className="-mt-[80px]">
         <Footer />

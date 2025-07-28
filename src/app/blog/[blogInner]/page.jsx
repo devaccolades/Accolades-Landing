aef6aebc7d7713798d64867f4bf6870600a0cc09
Afnan Accolades <ahammedafnan.accolades@gmail.com>
2025-07-28 17:53:51 +0530

    finished blogInner page

--- src/app/blog/[blogInner]/page.jsx
+++ src/app/blog/[blogInner]/page.jsx
@@ -1,9 +1,15 @@
+import Navbar from "@/component/Navbar";
+import BlogPost from "./BlogPost";
 import HeroSection from "./HeroSection";
+import Footer from "@/component/Footer";
 
 export default function page() {
   return (
     <>
+      <Navbar />
       <HeroSection />
+      <BlogPost />
+      <Footer />
     </>
   )
 }

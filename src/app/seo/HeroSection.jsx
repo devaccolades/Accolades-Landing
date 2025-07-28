233a7f178a1ec7a8c2d09cd295f92d111c1102c5
Afnan Accolades <ahammedafnan.accolades@gmail.com>
2025-07-28 11:17:06 +0530

    working on seo page

---
+++ src/app/seo/HeroSection.jsx
@@ -0,0 +1,30 @@
+export default function HeroSection() {
+  return (
+    <div className="lg:py-16 md:py-12 py-8">
+      <div className="bg-white containers rounded-[30px] py-6">
+        <h1 className="text-[#3FB4BA] font-mont font-bold xl:text-[70px] lg:text-[50px] md:text-[40px] text-[30px] leading-[130%] text-center mb-2">
+          SEO
+        </h1>
+      </div>
+      <div className="containers">
+          <div className="w-full">
+            <div className="bg-white rounded-[30px] px-6 md:px-8 lg:px-10 py-6 md:py-8 lg:py-10 mt-6">
+              <p className="font-mont font-normal md:text-[24px] text-[20px] leading-[130%] -tracking-[2%] text-[#333333] uppercase">
+                WHY CHOOSE OUR SEO SERVICE?
+              </p>
+
+              <h2 className="font-mont font-bold lg:text-[32px] md:text-[30px] text-[26px] leading-[130%] text-[#3FB4BA] mb-4 md:mb-6">
+                Search Engine Optimization
+              </h2>
+
+              <div className="font-mont font-normal xl:text-[18px] lg:text-[16px] md:text-[14px] text-[12px] leading-[150%] text-[#000000] space-y-4">
+                <p>
+                  Our team, Accolades Media the digital marketing agency in Kochi possesses the top Search Engine Optimization experts, who are professionally specialized, in Search Engine Optimization Services, to elevate our prospective clientele to the front runner in any search engine, by prioritizing the appropriate keywords that are suitable to generate the potential traffic to experience enhanced webpage visibility. The add-on advantage of receiving the professional guidance of a Search Engine Optimizer like us is to uplift and prioritize the randomly gathered contents from the internet.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+}

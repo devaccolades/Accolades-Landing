128c4974a305d8b388afeb90b594a093c524e59b
Afnan Accolades <ahammedafnan.accolades@gmail.com>
2025-07-28 14:33:42 +0530

    completed seo page

--- src/app/services/seo/Process.jsx
+++ src/app/services/seo/Process.jsx
@@ -1,14 +1,14 @@
 "use client";
 import Image from "next/image";
-import img18 from '../../../public/seo/image18.svg'
-import img19 from '../../../public/seo/image19.svg'
-import img20 from '../../../public/seo/image20.svg'
-import img21 from '../../../public/seo/image21.svg'
-import img22 from '../../../public/seo/image22.svg'
-import img23 from '../../../public/seo/image23.svg'
-import img24 from '../../../public/seo/image24.svg'
-import img26 from '../../../public/seo/image26.svg'
-import img27 from '../../../public/seo/image27.svg'
+import img18 from '../../../../public/seo/image18.svg'
+import img19 from '../../../../public/seo/image19.svg'
+import img20 from '../../../../public/seo/image20.svg'
+import img21 from '../../../../public/seo/image21.svg'
+import img22 from '../../../../public/seo/image22.svg'
+import img23 from '../../../../public/seo/image23.svg'
+import img24 from '../../../../public/seo/image24.svg'
+import img26 from '../../../../public/seo/image26.svg'
+import img27 from '../../../../public/seo/image27.svg'
 
 const seoSteps = [
   {
@@ -51,23 +51,21 @@ const seoSteps = [
 
 export default function Process() {
   return (
-    <section className="py-16">
+    <section className="lg:py-12 md:py-10 py-8 ">
       <div className="containers text-center">
-        <h2 className="font-mont font-bold xl:text-[40px] lg:text-[34px] md:text-[28px] text-[22px] leading-[130%] text-[#3FB4BA] mb-12">
+        <h2 className="font-mont font-bold xl:text-[40px] lg:text-[34px] md:text-[28px] text-[22px] leading-[130%] text-[#3FB4BA] mb-4 md:mb-8 lg:mb-12">
           Our SEO Process
         </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 md:gap-4 lg:gap-6">
           {seoSteps.map((step, index) => (
             <div
               key={index}
-              className="bg-white rounded-xl shadow-md p-6 flex flex-col items-center text-center transition hover:shadow-lg"
+              className="bg-white rounded-xl shadow-md p-6 flex flex-col items-center text-center transition hover:shadow-2xl"
             >
               <Image
                 src={step.icon}
                 alt={step.title}
-                width={80}
-                height={80}
-                className="mb-4"
+                className="mb-4 lg:size-[80px] md:size-[70px] size-[60px]"
               />
               <p className="font-mont font-semibold xl:text-[28px] leading-[100%] text-[#2C99B1] ">{step.title}</p>
             </div>

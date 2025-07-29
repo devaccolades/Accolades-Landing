eba4558a15562d1f37ffd2bc123f3ab6b0b7802d
manjimanm <manjimanm001@gmail.com>
2025-07-29 14:20:21 +0530

    wordpress, fullstack creation

---
+++ src/app/services/web-development/wordpress/Word-Hero.jsx
@@ -0,0 +1,48 @@
+
+
+"use client";
+import Image from "next/image";
+import React from "react";
+import Hero from "../../../../../public/web/word.png";
+
+export default function WordpressHero() {
+  return (
+    <div className="py-6 md:py-12 px-4 md:px-10 lg:px-20">
+      <div className="mx-auto max-w-7xl">
+        {/* Header */}
+        <div className="bg-white rounded-[30px] py-6 px-4">
+          <h1 className="text-[#3FB4BA] font-mont font-bold text-center leading-[130%]  text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-[70px]">
+            Wordpress Pages
+          </h1>
+        </div>
+
+        {/* Content Section */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8 lg:gap-8 mt-10 items-center">
+          {/* Right Image First on Mobile */}
+          <div className="flex justify-center  order-1 md:order-2">
+            <Image
+              src={Hero}
+              alt="Web Development Illustration"
+              className="object-contain w-64 md:w-96 lg:w-[500px] h-auto"
+              priority
+            />
+          </div>
+
+          {/* Left Text Second on Mobile */}
+          <div className="bg-white rounded-2xl shadow-md p-2 md:p-4 order-2 md:order-1">
+           
+            <h2 className="text-[20px] lg:text-[30px] xl:text-[40px] leading-[130%] font-bold font-mont text-[#1eb2a6] mb-2">
+             Custom WordPress Solutions for All Needs
+            </h2>
+            <p className="text-gray-700 font-mont text-[12px] lg:text-[14px] xl:text-[16px] leading-[130%]">
+             From lightning-fast blogs to stunning landing pages — we make WordPress work for you.
+            </p>
+            <button className="mt-4 px-6 py-2 bg-[#4bb9af] text-white rounded-4xl font-mont font-semibold hover:bg-[#17a19b] transition-colors">
+             Get a Creative Proposal
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

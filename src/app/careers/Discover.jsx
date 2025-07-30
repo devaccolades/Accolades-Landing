ee9b4b34730aa9fad96a7f552354734a99b4c48a
adil-accolades <adil.accolades@gmail.com>
2025-07-30 17:57:44 +0530

    some changes in the form

---
+++ src/app/careers/Discover.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+function Discover() {
+  return (
+    <section className="pt-8 md:pt-14">
+      <div className="w-[90%] md:w-[80%] rounded-2xl bg-white/60 mx-auto mt-18  relative z-10  p-[15px] md:p-[40px]">
+        <div className="">
+          <h1 className="text-center lg:text-left text-[#3FB4BA] font-bold text-[25px] md:text-[30px] font-mont leading-[130%] mb-[11px]">Discover Thrilling Opportunities</h1>
+          <p className="text-[14px] md:text-[16px] font-normal text-center lg:text-left">
+            Lorem ipsum dolor sit amet consectetur. Eget porttitor elementum
+            hendrerit a diam suspendisse tellus. Consequat enim auctor dignissim
+            mattis vitae tincidunt. Facilisis mi neque tellus viverra ac mauris
+            adipiscing nulla. Praesent purus ornare non eros et .
+          </p>
+        </div>
+        <div className="flex flex-col  justify-center items-center mt-[36px] gap-[20px]">
+          <div className="flex flex-col gap-[20px] bg-white rounded-lg p-[15px] lg:p-[30px] lg:w-full">
+            <div className="flex justify-between items-center">
+              <div>
+                <h2 className="font-semibold" >UI/UX Designer</h2>
+                <p className="text-[#3F3F3F] ">Kochi</p>
+              </div>
+              <button className="bg-[#3FB4BA] text-white rounded-full px-[15px] py-[7px] md:px-[40px] md:py-[14px]">Apply Now</button>
+            </div>
+            <div className="max-w-5xl">
+              Lorem ipsum dolor sit amet consectetur. Viverra justo dignissim
+              purus euismod potenti eu. Auctor pellentesque enim et ligula neque
+              sollicitudin elit sed sit.
+            </div>
+          </div>
+          <div className="flex flex-col gap-[20px] bg-white rounded-lg p-[15px] lg:p-[30px] lg:w-full">
+            <div className="flex justify-between items-center">
+              <div>
+                <h2 className="font-semibold" >UI/UX Designer</h2>
+                <p className="text-[#3F3F3F] ">Kochi</p>
+              </div>
+              <button className="bg-[#3FB4BA] text-white rounded-full px-[15px] py-[7px] md:px-[40px]  md:py-[14px]">Apply Now</button>
+            </div>
+            <div className="max-w-5xl">
+              Lorem ipsum dolor sit amet consectetur. Viverra justo dignissim
+              purus euismod potenti eu. Auctor pellentesque enim et ligula neque
+              sollicitudin elit sed sit.
+            </div>
+          </div>
+          <div className="flex flex-col gap-[20px] bg-white rounded-lg p-[15px] lg:p-[30px] lg:w-full">
+            <div className="flex justify-between items-center">
+              <div>
+                <h2 className="font-semibold" >UI/UX Designer</h2>
+                <p className="text-[#3F3F3F] ">Kochi</p>
+              </div>
+              <button className="bg-[#3FB4BA] text-white rounded-full px-[15px] py-[7px] md:px-[40px]  md:py-[14px]">Apply Now</button>
+            </div>
+            <div className="max-w-5xl">
+              Lorem ipsum dolor sit amet consectetur. Viverra justo dignissim
+              purus euismod potenti eu. Auctor pellentesque enim et ligula neque
+              sollicitudin elit sed sit.
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Discover;

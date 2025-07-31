58824321b6f0faeb8b748d5c733f8129fccc4650
Accolades Integrated <accoladesintegrated@Accoladess-iMac.local>
2025-07-31 17:52:39 +0530

    updated

--- src/app/services/digital-marketing/seo/Process.jsx
+++ src/app/services/digital-marketing/seo/Process.jsx
@@ -1,14 +1,14 @@
 "use client";
 import Image from "next/image";
-import img18 from '../../../../../public/seo/image18.svg'
-import img19 from '../../../../../public/seo/image19.svg'
-import img20 from '../../../../../public/seo/image20.svg'
-import img21 from '../../../../../public/seo/image21.svg'
-import img22 from '../../../../../public/seo/image22.svg'
-import img23 from '../../../../../public/seo/image23.svg'
-import img24 from '../../../../../public/seo/image24.svg'
-import img26 from '../../../../../public/seo/image26.svg'
-import img27 from '../../../../../public/seo/image27.svg'
+import img18 from "../../../../../public/seo/image18.svg";
+import img19 from "../../../../../public/seo/image19.svg";
+import img20 from "../../../../../public/seo/image20.svg";
+import img21 from "../../../../../public/seo/image21.svg";
+import img22 from "../../../../../public/seo/image22.svg";
+import img23 from "../../../../../public/seo/image23.svg";
+import img24 from "../../../../../public/seo/image24.svg";
+import img26 from "../../../../../public/seo/image26.svg";
+import img27 from "../../../../../public/seo/image27.svg";
 
 const seoSteps = [
   {
@@ -51,9 +51,9 @@ const seoSteps = [
 
 export default function Process() {
   return (
-    <section className="lg:py-12 md:py-10 py-8 ">
+    <section className="lg:pb-12 md:pb-10 pb-8 ">
       <div className="containers text-center">
-        <h2 className="font-mont font-bold xl:text-[40px] lg:text-[34px] md:text-[28px] text-[22px] leading-[130%] text-[#3FB4BA] mb-4 md:mb-8 lg:mb-12">
+        <h2 className="font-mont font-bold xl:text-[40px] lg:text-[34px] md:text-[28px] text-[22px] leading-[130%] text-[#3FB4BA] mb-4 ">
           Our SEO Process
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 md:gap-4 lg:gap-6">
@@ -67,7 +67,9 @@ export default function Process() {
                 alt={step.title}
                 className="mb-4 lg:size-[80px] md:size-[70px] size-[60px]"
               />
-              <p className="font-mont font-semibold xl:text-[28px] leading-[100%] text-[#2C99B1] ">{step.title}</p>
+              <p className="font-mont font-semibold xl:text-[28px] leading-[100%] text-[#2C99B1] ">
+                {step.title}
+              </p>
             </div>
           ))}
         </div>

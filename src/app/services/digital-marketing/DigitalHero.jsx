ed99883fdd6256a092bdd71c4fac5e8dac16d360
manjimanm <manjimanm001@gmail.com>
2025-08-01 17:34:36 +0530

    digital marketing page

---
+++ src/app/services/digital-marketing/DigitalHero.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import side from "../../../../public/digital/digi.jpg";
+import Image from "next/image";
+
+const DigitalHero = () => {
+  return (
+    <section className="containers pt-[100px]">
+      <h1
+        className="bg-white text-center text-[#3FB4BA] text-[36px] md:text-[50px] lg:text-[64px] font-mont py-5 rounded-[30px] font-bold
+        leading-[100%]"
+      >
+        Digital Marketing
+      </h1>
+      <div className="flex flex-col-reverse md:grid md:grid-cols-[60%_1fr] py-[5%] items-center gap-5">
+        <div className="flex flex-col font-mont bg-white p-5 lg:p-8 rounded-[30px] lg:space-y-3 space-y-2">
+          <h3 className="text-[#3FB4BA] text-[24px] lg:text-[40px] font-extrabold leading-[120%]">
+            Bringing Ideas to Life with Design That Inspires
+          </h3>
+          <h3 className="text-[16px] lg:text-[20px] text-[#333333] leading-[110%] font-normal">
+            Fuel your growth with scroll-stopping visuals, thumb-stopping
+            videos, and packaging that leaps off the shelf.
+          </h3>
+          <p className="text-[13px] md:text-[16px]  leading-[156%]">
+            Whether you’re launching a startup or refreshing an iconic brand,
+            our creative squad blends art and strategy to turn attention into
+            action.
+          </p>
+        </div>
+        <div>
+          <Image src={side} alt="side-image" className="w-full h-auto" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default DigitalHero;

a38302329a496d4e4d92fa2debe249f675e0ea38
Accolades Integrated <accoladesintegrated@Accoladess-iMac.local>
2025-08-04 10:14:09 +0530

    updated

--- src/app/services/web-development/fullstack-development/FullstackHero.jsx
+++ src/app/services/web-development/fullstack-development/FullstackHero.jsx
@@ -1,11 +1,8 @@
-
-
 "use client";
 import Image from "next/image";
 import React from "react";
 import Hero from "../../../../../public/web/fullstack.jpg";
-import Link from "next/link"
-
+import Link from "next/link";
 
 export default function FullstackHero() {
   return (
@@ -14,7 +11,7 @@ export default function FullstackHero() {
         {/* Header */}
         <div className="bg-white rounded-[30px] py-6 px-4">
           <h1 className="text-[#3FB4BA] font-mont font-bold text-center leading-[130%]  text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-[70px]">
-            Full Stack Web Development
+            Full-Stack Web Development
           </h1>
         </div>
 
@@ -32,18 +29,18 @@ export default function FullstackHero() {
 
           {/* Left Text Second on Mobile */}
           <div className="bg-white rounded-2xl shadow-md p-2 md:p-4 order-2 md:order-1">
-           
             <h2 className="text-[20px] lg:text-[30px] xl:text-[40px] leading-[130%] font-bold font-mont text-[#1eb2a6] mb-2">
-             End-to-End Web Development Solutions
+              End-to-End Web Development Solutions
             </h2>
             <p className="text-gray-700 font-mont text-[12px] lg:text-[14px] xl:text-[16px] leading-[130%]">
-             Scalable, secure, and modern web applications — custom-coded to match your business.
+              Scalable, secure, and modern web applications — custom-coded to
+              match your business.
             </p>
-           <Link href="/contact-us">
-        <button className="mt-4 px-6 py-2 bg-[#4bb9af] text-white rounded-4xl font-mont font-semibold hover:bg-[#17a19b] transition-colors">
-          Get a Creative Proposal
-        </button>
-      </Link>
+            <Link href="/contact-us">
+              <button className="mt-4 px-6 py-2 bg-[#4bb9af] text-white rounded-4xl font-mont font-semibold hover:bg-[#17a19b] transition-colors">
+                Get a Creative Proposal
+              </button>
+            </Link>
           </div>
         </div>
       </div>

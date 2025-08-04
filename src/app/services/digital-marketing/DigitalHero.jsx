07cc9fbfad817079cfa7f1fcc604bbe50247bc1e
manjimanm <manjimanm001@gmail.com>
2025-08-04 10:08:14 +0530

    digital title

--- src/app/services/digital-marketing/DigitalHero.jsx
+++ src/app/services/digital-marketing/DigitalHero.jsx
@@ -14,12 +14,9 @@ const DigitalHero = () => {
       <div className="flex flex-col-reverse md:grid md:grid-cols-[60%_1fr] py-[5%] items-center gap-5">
         <div className="flex flex-col font-mont bg-white p-5 lg:p-8 rounded-[30px] lg:space-y-3 space-y-2">
           <h3 className="text-[#3FB4BA] text-[24px] lg:text-[40px] font-extrabold leading-[120%]">
-            Bringing Ideas to Life with Design That Inspires
+            Full-Spectrum Digital Marketing Services: Target. Engage. Convert.
           </h3>
-          {/* <h3 className="text-[16px] lg:text-[20px] text-[#333333] leading-[110%] font-normal">
-            Fuel your growth with scroll-stopping visuals, thumb-stopping
-            videos, and packaging that leaps off the shelf.
-          </h3> */}
+          
           <p className="text-[13px] md:text-[16px]  leading-[156%]">
             At Accolades Integrated, we harness the power of digital to transform brands and accelerate business growth.
            Our expert team blends strategy, creativity, and technology to deliver performance-driven digital marketing 

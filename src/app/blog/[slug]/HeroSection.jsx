7f44e7e3cf91082862594e70004a3cc31f0d3828
adil-accolades <adil.accolades@gmail.com>
2025-08-07 10:03:58 +0530

    some chages in the frontend server

---
+++ src/app/blog/[slug]/HeroSection.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Img9 from "../../../../public/blog/heroImg.png";
+import { BASE_URL } from "@/app/Server";
+
+export default function HeroSection({ data }) {
+  return (
+    <>
+      <div className="bg-gradient-to-br from-white to-[#e6f4f9] pt-12 mt-20 ">
+        <div className="containers w-full h-full bg-white p-3 rounded-2xl">
+          <Image
+            src={BASE_URL + data?.innerImage.formats.medium.url}
+            height={100}
+            width={100}
+            alt="hero-img"
+            className="w-full h-[300px] md:h-[600px] rounded-2xl object-cover"
+          />
+        </div>
+      </div>
+    </>
+  );
+}

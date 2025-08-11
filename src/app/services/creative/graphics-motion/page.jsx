b14fd2f62d6b2d503a9a7b6bc2c2b839387a2748
adil-accolades <adil.accolades@gmail.com>
2025-08-11 16:13:17 +0530

    some git changes

--- src/app/services/creative/graphics-motion/page.jsx
+++ src/app/services/creative/graphics-motion/page.jsx
@@ -4,6 +4,29 @@ import HeroSection from "./HeroSection";
 import Platforms from "./Platforms";
 import Services from "./Services";
 import Navbar from "@/component/Navbar";
+import { BASE_URL, getSeo } from "./Server";
+
+export const dynamic = "force-dynamic";
+
+export  async function generateMetadata() {
+  const name = "graphic-designing";
+
+  // fetch post information
+  const post = await getSeo(name);
+  console.log("post", post[0].metaTitle);
+
+  return {
+    title: post[0]?.metaTitle,
+    description: post[0]?.metaDescription,
+    openGraph: {
+      title: post[0]?.ogTitle,
+      description: post[0]?.ogDescription,
+      images: [
+        BASE_URL + post[0]?.ogImage?.formats?.medium?.url, // Make sure this is a full URL to the image
+      ],
+    },
+  };
+}
 
 export default function page() {
   return (

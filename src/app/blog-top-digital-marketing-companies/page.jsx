f7aee78037472845da5b7d3aeea2d36a9793c09c
aswin <aswink.accolades@gmail.com>
2025-08-11 16:57:14 +0530

    blog

--- src/app/blog-top-digital-marketing-companies/page.jsx
+++ src/app/blog-top-digital-marketing-companies/page.jsx
@@ -1,11 +1,10 @@
 import Navbar from "@/component/Navbar";
 import Footer from "@/component/Footer";
 import BlogThings from "./BlogThings";
-import { getBlogs } from "../Server";
+
 
 export default async function page() {
   const data = await getBlogs();
-  console.log("data",data)
   return (
     <>
       <Navbar />

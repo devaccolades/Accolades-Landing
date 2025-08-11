8c7b66f1bbe6d59d54108787925fd395dbb6056f
adil-accolades <adil.accolades@gmail.com>
2025-08-11 16:13:17 +0530

    some git changes

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,11 +1,32 @@
 import Navbar from "@/component/Navbar";
 import Footer from "@/component/Footer";
 import BlogThings from "./BlogThings";
-import { getBlogs } from "../Server";
+import { BASE_URL, getBlogs, getSeo } from "../Server";
+
+export const dynamic = "force-dynamic";
+
+export  async function generateMetadata() {
+  const name = "blog";
+
+  // fetch post information
+  const post = await getSeo(name);
+  console.log("post", post[0].metaTitle);
+
+  return {
+    title: post[0]?.metaTitle,
+    description: post[0]?.metaDescription,
+    openGraph: {
+      title: post[0]?.ogTitle,
+      description: post[0]?.ogDescription,
+      images: [
+        BASE_URL + post[0]?.ogImage?.formats?.medium?.url, // Make sure this is a full URL to the image
+      ],
+    },
+  };
+}
 
 export default async function page() {
   const data = await getBlogs();
-  console.log("data",data)
   return (
     <>
       <Navbar />
--- src/app/services/digital-marketing/content-marketing/page.jsx
+++ src/app/services/digital-marketing/content-marketing/page.jsx
@@ -1,6 +1,29 @@
 import Navbar from "../../../../component/Navbar";
 import HeroSection from "./HeroSection";
 import Footer from '../../../../component/Footer'
+import { BASE_URL, getSeo } from "@/app/Server";
+
+export const dynamic = "force-dynamic";
+
+export async function generateMetadata() {
+  const name = "content-marketing";
+
+  // fetch post information
+  const post = await getSeo(name);
+  console.log("post", post[0].metaTitle);
+
+  return {
+    title: post[0]?.metaTitle,
+    description: post[0]?.metaDescription,
+    openGraph: {
+      title: post[0]?.ogTitle,
+      description: post[0]?.ogDescription,
+      images: [
+        BASE_URL + post[0]?.ogImage?.formats?.medium?.url, // Make sure this is a full URL to the image
+      ],
+    },
+  };
+}
 
 export default function page() {
   return (

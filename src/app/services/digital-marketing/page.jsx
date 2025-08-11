c4ae61f3ad6bc1492c1d3cae93b4239d5b4e77ad
adil-accolades <adil.accolades@gmail.com>
2025-08-11 15:00:07 +0530

    seo changes

--- src/app/services/digital-marketing/page.jsx
+++ src/app/services/digital-marketing/page.jsx
@@ -1,28 +1,51 @@
-import Navbar from '@/component/Navbar'
-import React from 'react'
-import DigitalHero from './DigitalHero'
-import WhatWeDo from './WhatWeDo'
-import Tools from './Tools'
-import WhyChoose from './WhyChoose'
-import OurClients from './OurClients'
-import Footer from '@/component/Footer'
+import Navbar from "@/component/Navbar";
+import React from "react";
+import DigitalHero from "./DigitalHero";
+import WhatWeDo from "./WhatWeDo";
+import Tools from "./Tools";
+import WhyChoose from "./WhyChoose";
+import OurClients from "./OurClients";
+import Footer from "@/component/Footer";
+import { BASE_URL, getSeo } from "../Server";
+
+export const dynamic = "force-dynamic";
+
+export async function generateMetadata() {
+  const name = "digital-marketing";
+
+  // fetch post information
+  const post = await getSeo(name);
+  console.log("post", post[0].metaTitle);
+
+  return {
+    title: post[0]?.metaTitle,
+    description: post[0]?.metaDescription,
+    openGraph: {
+      title: post[0]?.ogTitle,
+      description: post[0]?.ogDescription,
+      images: [
+        BASE_URL + post[0]?.ogImage?.formats?.medium?.url, // Make sure this is a full URL to the image
+      ],
+    },
+  };
+}
 
 function page() {
   return (
     <div>
-        <main className="bg-[#F1F1F1] ">
-      <Navbar />
-      <DigitalHero />
-      <WhatWeDo />
-      <OurClients />
-      <WhyChoose />
-      <Tools />
+      <main className="bg-[#F1F1F1] ">
+        <Navbar />
+        <DigitalHero />
+        <WhatWeDo />
+        <OurClients />
+        <WhyChoose />
+        <Tools />
       </main>
-       <main className="-mt-[65px]">
+      <main className="-mt-[65px]">
         <Footer />
       </main>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;

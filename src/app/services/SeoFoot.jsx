d2eb493f565f23ab7cb130503019567b9d6c13ac
manjimanm <manjimanm001@gmail.com>
2025-11-25 15:34:36 +0530

    seoEXtra contents in SeoFoot.jx

---
+++ src/app/services/SeoFoot.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function SeoFoot() {
+  return (
+    <section className="h-[400px] md:h-[400px] w-full pb-30 pt-2 md:pt-24 lg:pt-3 mb-60 md:mb-14">
+      <div className="flex flex-col items-center justify-center bg-white w-[90%] mx-auto p-4 rounded-2xl relative z-10">
+        <h2 className="font-mont text-[20px] md:text-[30px] lg:text-[36px] text-center font-bold leading-[130%] text-[#3FB4BA]">
+          The Accolades Legacy
+        </h2>
+        <p className="max-w-7xl font-mont text-left mt-[30px] text-[14px] md:text-[16px] leading-[150%] ">
+          Accolades Integrated has evolved into a leading force in the digital and creative industry. Since 2012,
+          our commitment to innovation and client success has helped hundreds of brands across India grow
+          their presence.
+          We serve clients in Kochi, Mumbai, Calicut, Trivandrum, and Bengaluru, offering end-to-end
+          marketing and web solutions tailored to local and national markets. As one of the most recognized
+          names in digital marketing services Kochi, our mission is to make every brand more visible, valuable,
+          and memorable.
+          Our legacy is built on measurable performance, creative excellence, and long-term partnerships —
+          because when our clients grow, we grow with them.
+
+        </p>
+
+
+
+      </div>
+    </section>
+  );
+}
+
+export default SeoFoot;

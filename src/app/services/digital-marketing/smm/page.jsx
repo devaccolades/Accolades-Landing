1b5c3b2eb71f1c00f86386dd0503d7355b29e698
manjimanm <manjimanm001@gmail.com>
2025-12-10 13:31:40 +0530

    baseurl api commented

--- src/app/services/digital-marketing/smm/page.jsx
+++ src/app/services/digital-marketing/smm/page.jsx
@@ -3,7 +3,7 @@ import Navbar from "@/component/Navbar";
 import React from "react";
 import HeroSection from "./HeroSection";
 import Details from "./Details";
-import { BASE_URL, getSeo } from "@/app/Server";
+// import { BASE_URL, getSeo } from "@/app/Server";
 import SocialMediaHero from "./SmmHero";
 
 import WhyInvestSMM from "./WhyInvestSMM";
@@ -15,26 +15,25 @@ import SmmFaq from "./SmmFaq";
 
 export const dynamic = "force-dynamic";
 
-export async function generateMetadata() {
-  const name = "smm";
+// export async function generateMetadata() {
+//   const name = "smm";
 
-  // fetch post information
-  const post = await getSeo(name);
-  // console.log("post", post[0].metaTitle);
+  
+//   const post = await getSeo(name);
 
-  // return {
-  //   title: post[0]?.metaTitle,
-  //   description: post[0]?.metaDescription,
-  //   keywords:"Social Media Marketing Company in Kochi",
-  //   openGraph: {
-  //     title: post[0]?.ogTitle,
-  //     description: post[0]?.ogDescription,
-  //     images: [
-  //       BASE_URL + post[0]?.ogImage?.formats?.medium?.url, // Make sure this is a full URL to the image
-  //     ],
-  //   },
-  // };
-}
+//   return {
+//     title: post[0]?.metaTitle,
+//     description: post[0]?.metaDescription,
+//     keywords:"Social Media Marketing Company in Kochi",
+//     openGraph: {
+//       title: post[0]?.ogTitle,
+//       description: post[0]?.ogDescription,
+//       images: [
+//         BASE_URL + post[0]?.ogImage?.formats?.medium?.url, // Make sure this is a full URL to the image
+//       ],
+//     },
+//   };
+// }
 
 
 
--- src/app/services/web-development/fullstack-development/page.jsx
+++ src/app/services/web-development/fullstack-development/page.jsx
@@ -5,32 +5,31 @@ import WhatWeCover from './WeCover'
 import Footer from '@/component/Footer'
 import FullstackProjects from './FullstackProjects'
 
-import { BASE_URL, getSeo } from "@/app/Server";
+// import { BASE_URL, getSeo } from "@/app/Server";
 import PythonHero from './PythonHero'
 import TechnologyStack from './TechnologyStack'
 
 export const dynamic = "force-dynamic";
 
-export async function generateMetadata() {
-  const name = "fullstack-development";
+// export async function generateMetadata() {
+//   const name = "fullstack-development";
 
-  // fetch post information
-  const post = await getSeo(name);
-  // console.log("post", post[0].metaTitle);
+//   // fetch post information
+//   const post = await getSeo(name);
 
-  // return {
-  //   title: post[0]?.metaTitle,
-  //   description: post[0]?.metaDescription,
-  //    keywords:"Python Web Developers in Kochi ",
-  //   openGraph: {
-  //     title: post[0]?.ogTitle,
-  //     description: post[0]?.ogDescription,
-  //     images: [
-  //       BASE_URL + post[0]?.ogImage?.formats?.medium?.url, // Make sure this is a full URL to the image
-  //     ],
-  //   },
-  // };
-}
+//   return {
+//     title: post[0]?.metaTitle,
+//     description: post[0]?.metaDescription,
+//      keywords:"Python Web Developers in Kochi ",
+//     openGraph: {
+//       title: post[0]?.ogTitle,
+//       description: post[0]?.ogDescription,
+//       images: [
+//         BASE_URL + post[0]?.ogImage?.formats?.medium?.url, // Make sure this is a full URL to the image
+//       ],
+//     },
+//   };
+// }
 
 function page() {
   return (
